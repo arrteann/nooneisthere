73a0fd0adefc5cfe22fd6b3ad212bed568144477
Artin <artindev@gmail.com>
2019-08-20 17:19:51 +0430

    Change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage>{
+class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
-      body: WebviewScaffold(
-        url: 'http://nooneisthere.ir',
-        withJavascript: true,
-        
-      )
-    );
+        body: WebviewScaffold(
+      url: 'http://nooneisthere.ir',
+      withJavascript: true,
+      supportMultipleWindows: true,
+      withLocalStorage: true,
+      allowFileURLs: true,
+      enableAppScheme: true,
+      appCacheEnabled: true,
+      hidden: false,
+      scrollBar: true,
+      geolocationEnabled: false,
+    ));
   }
 }
